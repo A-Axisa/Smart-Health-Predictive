94f6fd67f4cb166e8049d76767f0b95250888595
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-09-10 10:23:35 +0930

    chore: Remove welcome text panel

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -31,30 +31,11 @@ const Register = ({}) => {
     <Box sx={{ backgroundColor:'#127067',  width:'100vw', height:'100vh', 
         padding:'0', margin:'0'}}>
 
-      <Box sx={{ backgroundImage:'linear-gradient(to top left, #133a37ff, #127067)',
-          width:'60%', height:'100vh', flex:'inline', float:'left'}}>
-        <Container style={{padding:'100px'}}>
-          <Stack>
-            <Typography style={{ color:"#e7f1f1ff", fontWeight:'bold', 
-                fontSize:'100px' }}>Welcome</Typography>
-            <Typography style={{ color:"#cae4e4ff" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut 
-              facilisis arcu et ex tristique, ut hendrerit est posuere. Nunc 
-              fringilla commodo neque vel scelerisque. Sed id dictum massa, eu 
-              tristique enim. Vestibulum pellentesque quis nibh et egestas. 
-              Nulla efficitur quam et venenatis rhoncus. Quisque nec odio a 
-              ligula facilisis semper vel ut mi. Quisque ac nulla tortor. 
-              Curabitur egestas dictum risus, ac efficitur diam vulputate sed. 
-              Vivamus sed tortor nunc.
-            </Typography>
-          </Stack>  
-        </Container>
-      </Box>
-
-      <Box sx={{ backgroundColor:'#ffffff', width:'40%', height:'100%', 
-          flex:'inline', float:'left', display:'flex', alignItems:'center'}}>
-        <Container sx={{ width:'70%', borderRadius:2, padding:'25px', 
-            alignItems:'center', boxShadow:24}}>
+      <Box sx={{backgroundImage:'linear-gradient(to top left, #133a37ff, #127067)',
+          width:'100%', height:'100%', flex:'inline', float:'left', display:'flex',
+          alignItems:'center'}}>
+        <Container sx={{ backgroundColor:'#ffffff', width:'30%', borderRadius:2, 
+            padding:'25px', alignItems:'center', boxShadow:24}}>
           <Box component='form' onSubmit={handleRegistration}>
             <Stack spacing={{xs:2}}>
               <h1>Create Account</h1>
