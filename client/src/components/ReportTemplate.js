fc7feee31921c3202830e3fb2ae608ea9b4db32b
Luke Bradley <bralc003@mymail.unisa.edu.au>
2025-09-11 19:28:34 +0930

    Format each section of the health report

--- client/src/components/ReportTemplate.js
+++ client/src/components/ReportTemplate.js
@@ -1,67 +1,167 @@
 import {
-    Box,
-    Typography,
-    List,
-    ListItem,
-    ListItemText,
-    TextField,
-    Divider,
-    Button,
-    Card
+	Box,
+	Typography,
+	List,
+	ListItem,
+	ListItemText,
+	Divider,
+	Card,
+	Grid
 } from '@mui/material';
 
 
-const ReportTemplate = ({report}) => {
+const ReportTemplate = ({ report }) => {
 
-    return (
-        <Box>
-            <Box>
-                
-            <Typography variant="h3" sx={ {fontWeight: 600 }}>
-                Report {report.date }
-                </Typography>
-            </Box>
-                <Divider sx={{ borderColor: '#e0e0e0' }} />
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Stroke: (Low, 10%)
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Diabeties: (Low, 10%)
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                CVD: (Low, 10%)
-            </Typography>
-            <Divider sx={{ borderColor: '#e0e0e0' }} />
+	return (
+		<Box>
+			<Typography
+				variant="h3"
+				sx={{ fontWeight: 600, textAlign: "center", mt: 3, mb: 3, color: 'primary.main' }}>
+				Report {report.date}
+			</Typography>
+			<Divider sx={{ borderColor: '#e0e0e0' }} />
 
-            <Typography variant="h5">
-                Lifestyle Recomendations
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Text
-            </Typography>
+			<Typography variant="h4" sx={{ mb: 3, mt: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }}>
+				Health Information Summary
+			</Typography>
+			<Typography variant="body1" sx={{ mb: 3, ml: 5 }} >
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. Nunc
+				fringilla commodo neque vel scelerisque. Sed id dictum massa, eu
+				tristique enim. Vestibulum pellentesque quis nibh et egestas.
+				Nulla efficitur quam et venenatis rhoncus. Quisque nec odio a
+				ligula facilisis semper vel ut mi. Quisque ac nulla tortor.
+				Curabitur egestas dictum risus, ac efficitur diam vulputate sed.
+				Vivamus sed tortor nunc.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. Nunc
+				fringilla commodo neque vel scelerisque. Sed id dictum massa, eu
+				tristique enim. Vestibulum pellentesque quis nibh et egestas.
+				Nulla efficitur quam et venenatis rhoncus. Quisque nec odio a
+				ligula facilisis semper vel ut mi. Quisque ac nulla tortor.
+				Curabitur egestas dictum risus, ac efficitur diam vulputate sed.
+				Vivamus sed tortor nunc.
+			</Typography>
 
-            <Typography variant="h5">
-                Exercise Recomendations
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Text
-            </Typography>
+			<Divider sx={{ borderColor: '#e0e0e0' }} />
+			{/* Health Predictions */}
+			<Typography variant="h4" sx={{ mb: 3, mt: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }}>
+				Health Prediction
+			</Typography>
+			<Grid container spacing={2}>
+				<Grid size={4}>
+					<Typography variant="h6" sx={{ textAlign: "center" }} >
+						Stroke: {report.strokeChance}%
+					</Typography>
+				</Grid>
+				<Grid size={4}>
+					<Typography variant="h6" sx={{ textAlign: "center" }}>
+						Diabetes: {report.diabetesChance}%
+					</Typography>
+				</Grid>
+				<Grid size={4}>
+					<Typography variant="h6" sx={{ textAlign: "center" }}>
+						CVD: {report.cardioChance}%
+					</Typography>
+				</Grid>
+			</Grid>
 
-            <Typography variant="h5">
-                Diet Recomendations
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Text
-            </Typography>
+			<Divider sx={{ borderColor: '#e0e0e0' }} />
 
-            <Typography variant="h5">
-                Diet to Avoid
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-                Text
-            </Typography>
-            </Box>
-    );
+			<Grid container spacing={2}>
+				{/* Lifestyle Recomendations */}
+				<Grid size={6}>
+					<Card variant="outlined" sx={{ maxWidth: 800, margin: "2rem auto", padding: 2 }}>
+						<Typography variant="h5" sx={{ mb: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }} >
+							Lifestyle Recomendations
+						</Typography>
+						<List>
+							<ListItem>
+								<ListItemText primary="Recomendation 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. " />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+
+				{/* Exercise Recomendations*/}
+				<Grid size={6}>
+					<Card variant="outlined" sx={{ maxWidth: 800, margin: "2rem auto", padding: 2 }}>
+						<Typography variant="h5" sx={{ mb: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }} >
+							Exercise Recomendations
+						</Typography>
+						<List>
+							<ListItem>
+								<ListItemText primary="Recomendation 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. " />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+
+				{/* Diet Recomendations*/}
+				<Grid size={6}>
+					<Card variant="outlined" sx={{ maxWidth: 800, margin: "2rem auto", padding: 2 }}>
+						<Typography variant="h5" sx={{ mb: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }} >
+							Diet Recomendations
+						</Typography>
+						<List>
+							<ListItem>
+								<ListItemText primary="Recomendation 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. " />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+
+				{/* Diet to Avoid*/}
+				<Grid size={6}>
+					<Card variant="outlined" sx={{ maxWidth: 800, margin: "2rem auto", padding: 2 }}>
+						<Typography variant="h5" sx={{ mb: 3, color: 'primary.main', fontWeight: 600, textAlign: "center" }} >
+							Diet to Avoid
+						</Typography>
+						<List>
+							<ListItem>
+								<ListItemText primary="Recomendation 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere. " />
+							</ListItem>
+							<ListItem>
+								<ListItemText primary="Recomendation 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				facilisis arcu et ex tristique, ut hendrerit est posuere." />
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 }
 
 export default ReportTemplate
--- client/src/routes/AIHealthPrediction.js
+++ client/src/routes/AIHealthPrediction.js
@@ -1,85 +1,83 @@
 import ReportTemplate from "../components/ReportTemplate";
 import React, { useState } from 'react';
 import {
-    Box,
-    Typography,
-    List,
-    ListItem,
-    ListItemText,
-    TextField,
-    Divider,
-    Button,
-    Avatar,
-    Switch,
-    FormControlLabel,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Alert,
-    Stack,
+	Box,
+	Typography,
+	List,
+	ListItem,
+	ListItemText,
 } from '@mui/material';
 
 const AIHealthPrediction = ({ }) => {
 
-    // dummy report data
-    const report = [{
-        id: 0,
-        date: '11/09/2025'
-    },
-    {
-        id: 1,
-        date: '7/09/2025'
-    },
-    {
-        id: 2,
-        date: '3/09/2025'
-    },
-    ]
-    const [selectedReport, setSelectedReport] = useState(report[0]);
-    console.log(selectedReport)
+	// dummy report data
+	const report = [{
+		id: 0,
+		date: '11/09/2025',
+		strokeChance: 15,
+		cardioChance: 13,
+		diabetesChance: 0,
+	},
+	{
+		id: 1,
+		date: '7/09/2025',
+		strokeChance: 30,
+		cardioChance: 20,
+		diabetesChance: 10,
+	},
+	{
+		id: 2,
+		date: '3/09/2025',
+		strokeChance: 66,
+		cardioChance: 34,
+		diabetesChance: 12,
+	},
+	]
+	const [selectedReport, setSelectedReport] = useState(report[0]);
+	console.log(selectedReport)
 
-    return (
-        <Box sx={{ display: 'flex', minHeight: '100vh', bgcolor: 'background.default' }}>
-            {/* Sidebar */}
-            <Box sx={{ width: 400, bgcolor: 'background.paper', borderRight: '1px solid #e0e0e0' }}>
-                <Box sx={{ p: 3, borderBottom: '1px solid #e0e0e0' }}>
-                    <Typography variant="h6" sx={{ fontWeight: 600 }}>
-                        Report History
-                    </Typography>
-                </Box>
-                <List component="nav" sx={{ p: 0 }}>
-                    {report.map((item) =>
-                        <ListItem key={item.id} selected={selectedReport.id === item.id}
-                            onClick={() => setSelectedReport(item)}
-                            button
-                            sx={{
-                                py: 2,
-                                px: 3,
-                                borderLeft:
-                                    selectedReport.id === item.id ? '4px solid' : '4px solid transparent',
-                                borderLeftColor: 'primary.main',
-                                bgcolor: selectedReport.id === item.id ? 'action.selected' : 'transparent',
-                            }}
-                        >
-                            <ListItemText
-                                primary={`Report: ${item.date}`}
-                                slotProps={{
-                                    primary: {
-                                        style: {
-                                            fontWeight: selectedReport.id === item.id ? 600 : 400,
-                                        },
-                                    },
-                                }}
-                            />
-                        </ListItem>
-                    )}
-                </List>
-            </Box>
-            {/* Report Content */}
-            <ReportTemplate report = {selectedReport} />
-        </Box>
-    );
+	return (
+		<Box sx={{ display: 'flex', minHeight: '100vh', bgcolor: 'background.default' }}>
+			{/* Sidebar */}
+			<Box sx={{ width: 400, bgcolor: 'background.paper', borderRight: '1px solid #e0e0e0' }}>
+				<Box sx={{ p: 3, borderBottom: '1px solid #e0e0e0' }}>
+					<Typography variant="h6" sx={{ fontWeight: 600 }}>
+						Report History
+					</Typography>
+				</Box>
+				<List component="nav" sx={{ p: 0 }}>
+					{report.map((item) =>
+						<ListItem key={item.id} selected={selectedReport.id === item.id}
+							onClick={() => setSelectedReport(item)}
+							button
+							sx={{
+								py: 2,
+								px: 3,
+								borderLeft:
+									selectedReport.id === item.id ? '4px solid' : '4px solid transparent',
+								borderLeftColor: 'primary.main',
+								bgcolor: selectedReport.id === item.id ? 'action.selected' : 'transparent',
+							}}
+						>
+							<ListItemText
+								primary={`Report: ${item.date}`}
+								slotProps={{
+									primary: {
+										style: {fontWeight: selectedReport.id === item.id ? 600 : 400,
+										},
+									},
+								}}
+							/>
+						</ListItem>
+					)}
+				</List>
+			</Box>
+			{/* Report Content */}
+			<Box sx={{ flex: 1 }}>
+				<ReportTemplate report={selectedReport} />
+			</Box>
+		</Box>
+	);
 
 }
 
