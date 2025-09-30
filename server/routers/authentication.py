4bc75e810f5afa6c0f8e3e1b64f807cde7e95c51
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-09-30 21:27:28 +0930

    feat: Add account creation endpoint

---
+++ server/routers/authentication.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta, timezone
+from secrets import token_urlsafe
+
+import bcrypt
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, EmailStr
+from sqlalchemy.orm import Session
+
+from ..utils.database import get_db 
+from ..models.dbmodels import UserAccount
+
+class UserRegistrationDetails(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    phone: str
+
+router = APIRouter()
+
+@router.post("/register/")
+async def register(user_reg: UserRegistrationDetails, db_conn: Session = Depends(get_db)):
+
+    # Always hash the password to obfuscate success and failure.
+    password_hash = bcrypt.hashpw(user_reg.password.encode('utf-8'), bcrypt.gensalt(rounds=15))
+    new_user = UserAccount(
+        user_reg.username,
+        user_reg.email,
+        user_reg.password,
+        user_reg.phone
+    )
+
+    # Add the user if the email doesn't already exist.
+    user = db_conn.query(UserAccount).filter_by(Email=user_reg.email).first()
+    if not user:        
+        db_conn.add(new_user)
+        db_conn.commit()
+
+    return {'message': 'User successfully created.'}
