181c807c93cf1e23908d29be9c1d766d4ad8c726
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-02 23:38:20 +0930

    feat: Add fetch users endpoint

---
+++ server/routers/users.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import Session
+from ..utils.database import get_db
+from ..models.dbmodels import UserAccount
+
+
+router = APIRouter()
+
+@router.get("/users/")
+async def readUser(db_conn: Session = Depends(get_db)):
+    users = db_conn.query(UserAccount).all()
+    
+    result = []
+
+    for user in users:
+        result.append({
+            "id": user.UserID,
+            "fullName": user.FullName,
+            "email": user.Email,
+            "phoneNumber": user.PhoneNumber,
+            "createdAt": user.CreatedAt
+        })
+
+    return result
