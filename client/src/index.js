4127683fcce53d4e9cbea1102b26879941bb14f2
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-03 16:59:12 +0930

    chore: Update MerchantGenerateReport route

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import Login from './routes/Login';
 import Register from './routes/Register';
 import UserLanding from './routes/UserLanding';
 import UserSettings from './routes/UserSettings';
+import MerchantGenerateReport from './routes/MerchantGenerateReport';
 import AppThemeProvider from './components/AppThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -27,6 +28,7 @@ root.render(
           <Route path="/ai-health-prediction" element={ <AIHealthPrediction /> } />
           <Route path="/generate-report" element={ <GenerateReport/> } />
           <Route path="/health-analytics" element={ <HealthAnalytics /> } />
+          <Route path="/merchant-generate-report" element={ <MerchantGenerateReport />} />
           <Route path=""  element={ <App /> } />
           <Route path="*" element={ <App /> } />
         </Routes>
