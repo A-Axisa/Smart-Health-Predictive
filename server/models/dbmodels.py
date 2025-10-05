ed8f1889082a17d50a510510ff4c3c8c23bd2efb
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-05 17:32:44 +1030

    chore: Add role and permission table ORM models

--- server/models/dbmodels.py
+++ server/models/dbmodels.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime, text
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, text, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
-class UserAccount(declarative_base()):
+
+Base = declarative_base()
+
+class UserAccount(Base):
     __tablename__ = 'UserAccount'
     UserID = Column(Integer, primary_key = True)
     FullName = Column(String)
@@ -9,6 +12,8 @@ class UserAccount(declarative_base()):
     PasswordHash = Column(String)
     PhoneNumber = Column(String)
     CreatedAt = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+
+    userRoles = relationship("UserAccountRole", back_populates = "user")
 
     def __init__(self, full_name, email, password_hash, phone_number):
         self.FullName = full_name
@@ -20,4 +25,40 @@ class UserAccount(declarative_base()):
         return f"UserAccount(UserID={self.UserID}, FullName={self.FullName}, \
             Email={self.Email}, PasswordHash={self.PasswordHash}, \
             Phone={self.PhoneNumber}, Created={self.CreatedAt} )"
-    
+
+
+class AccountRole(Base):
+    __tablename__ = 'AccountRole'
+    RoleID = Column(Integer, primary_key = True)
+    RoleName = Column(String)
+
+    userRoles = relationship("UserAccountRole", back_populates = "role")
+    rolePermissions = relationship("RolePermission", back_populates = "role")
+
+
+class Permission(Base):
+    __tablename__ = 'Permission'
+    PermissionID = Column(Integer, primary_key = True)
+    PermissionName = Column(String)
+
+    permissionRoles = relationship("RolePermission", back_populates = "permission")    
+
+
+class UserAccountRole(Base):
+    __tablename__ = 'UserAccountRole'
+    RoleID = Column(Integer, ForeignKey("AccountRole.RoleID"), primary_key = True)
+    UserID = Column(Integer, ForeignKey("UserAccount.UserID"), primary_key = True)
+    AssignedAT = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+
+    user = relationship("UserAccount", back_populates = "userRoles")
+    role = relationship("AccountRole", back_populates = "userRoles")
+
+
+class RolePermission(Base):
+    __tablename__ = 'RolePermission'
+    RoleID = Column(Integer, ForeignKey("AccountRole.RoleID"), primary_key = True)
+    PermissionID = Column(Integer, ForeignKey("Permission.PermissionID"), primary_key = True)
+    AssignedAT = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+
+    role = relationship("AccountRole", back_populates = "rolePermissions")
+    permission = relationship("Permission", back_populates = "permissionRoles")
