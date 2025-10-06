67abe9d64d9a4528a55fbd8632d3e2118d711e7b
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-06 19:45:22 +1030

    chore: Add UI for ReportUpload component

--- client/src/components/ReportUpload.js
+++ client/src/components/ReportUpload.js
@@ -1,6 +1,16 @@
+import {Box, Button, Typography} from '@mui/material'
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+
 
 const ReportUpload = ({}) => {
-
+  return (
+    <Box sx={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
+      <Typography sx={{color: 'grey', mb: 4}}>Only PDF (.pdf) or Excel (.xlsx) files are accepted.</Typography>
+      <Button variant='contained' color='info' size='large'>
+        Upload File <FileUploadIcon/>
+      </Button>
+    </Box>
+  )
 }
 
 export default ReportUpload
