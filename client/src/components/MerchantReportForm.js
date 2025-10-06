fe98a3d2e3bfd394894f58faaf9ac20bcd988d7f
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-07 02:09:32 +1030

    chore: Add data handling funciton to MerchantReportForm

--- client/src/components/MerchantReportForm.js
+++ client/src/components/MerchantReportForm.js
@@ -3,10 +3,43 @@ RadioGroup, FormControlLabel, Radio, Paper, Typography,
 Button } from '@mui/material';
 
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+
+  console.log({
+    // Personal information
+    firstName: formData.get('firstName'),
+    middleName: formData.get('middleName'),
+    lastName: formData.get('lastName'),
+    email: formData.get('email'),
+
+    // Age & physique
+    age: formData.get('age'),
+    weight: formData.get('weight'),
+    height: formData.get('height'),
+    bloodGlucose: formData.get('bloodGlucose'),
+
+    // Fitness
+    exercise: formData.get('exercise'),
+    heartDisease: formData.get('heartDisease'),
+    hyperTension: formData.get('hyperTension'),
+    diabetes: formData.get('diabetes'),
+
+    // Lifestyle
+    alcohol: formData.get('alcohol'),
+    smoker: formData.get('smoker'),
+
+    // Life events
+    maritalStatus: formData.get('maritalStatus'),
+    workingStatus: formData.get('workingStatus')
+  });
+}
+
 const MerchantReportForm = ({}) => {
   return (
     <Paper elevation={3}>
-      <Box sx={{ p:1 }}>
+      <Box component="form" onSubmit={handleSubmit} sx={{ p:1 }}>
         <Grid container spacing={2}>
           <Grid item size={12} sx={{justifyContent: 'center', display: 'flex'}}>
             <Typography variant='h5' color='primary' sx={{fontWeight: '600'}}>
@@ -21,13 +54,13 @@ const MerchantReportForm = ({}) => {
               </Typography>
             </Grid>
             <Grid item xs={8}>
-              <TextField required fullWidth name='first-name' label='First Name' placeholder='John'/>
+              <TextField required fullWidth name='firstName' label='First Name' placeholder='John'/>
             </Grid>
             <Grid item xs={8}>
-              <TextField required fullWidth name='middle-name' label='Middle Name' placeholder='Doe'/>
+              <TextField required fullWidth name='middleName' label='Middle Name' placeholder='Doe'/>
             </Grid>
             <Grid item xs={4}>
-              <TextField required fullWidth name='last-name' label='Last Name' placeholder='Smith'/>
+              <TextField required fullWidth name='lastName' label='Last Name' placeholder='Smith'/>
             </Grid>
             <Grid item xs={12}>
               <TextField required name='email' label='Email' placeholder='firstname@example.com'/>
@@ -47,7 +80,7 @@ const MerchantReportForm = ({}) => {
               <TextField required name='height' label='Height (cm)'/>
             </Grid>
             <Grid item xs={6}>
-              <TextField required name='bloodglucose' label='Blood Glucose (mmol/L)'/>
+              <TextField required name='bloodGlucose' label='Blood Glucose (mmol/L)'/>
             </Grid>
 
             {/* Fitness */}
@@ -60,7 +93,7 @@ const MerchantReportForm = ({}) => {
             <Grid>
               <FormControl>
                 <FormLabel id="exercise">Exercise</FormLabel>
-                <RadioGroup row aria-labelledby="exercise" name="exercise-buttons">
+                <RadioGroup row aria-labelledby="exercise" name="exercise">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -70,8 +103,8 @@ const MerchantReportForm = ({}) => {
             {/* Heart disease */}
             <Grid>
               <FormControl>
-                <FormLabel id="heart-disease">Heart Disease</FormLabel>
-                <RadioGroup row aria-labelledby="heart-disease" name="heart-disease-buttons">
+                <FormLabel id="heartDisease">Heart Disease</FormLabel>
+                <RadioGroup row aria-labelledby="heartDisease" name="heartDisease">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -81,8 +114,8 @@ const MerchantReportForm = ({}) => {
             {/* Hyper tension */}
             <Grid>
               <FormControl>
-                <FormLabel id="hyper-tension">Hypter Tension</FormLabel>
-                <RadioGroup row aria-labelledby="hyper-tension" name="hyper-tension-buttons">
+                <FormLabel id="hyperTension">Hypter Tension</FormLabel>
+                <RadioGroup row aria-labelledby="hyperTension" name="hyperTension">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -93,7 +126,7 @@ const MerchantReportForm = ({}) => {
             <Grid>
               <FormControl>
                 <FormLabel id="diabetes">Diabetes</FormLabel>
-                <RadioGroup row aria-labelledby="diabetes" name="diabetes-buttons">
+                <RadioGroup row aria-labelledby="diabetes" name="diabetes">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -110,7 +143,7 @@ const MerchantReportForm = ({}) => {
             <Grid>
               <FormControl>
                 <FormLabel id="alcohol">Alcohol</FormLabel>
-                <RadioGroup row aria-labelledby="alcohol" name="alcohol-buttons">
+                <RadioGroup row aria-labelledby="alcohol" name="alcohol">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -121,7 +154,7 @@ const MerchantReportForm = ({}) => {
             <Grid>
               <FormControl>
                 <FormLabel id="smoker">Smoker</FormLabel>
-                <RadioGroup row aria-labelledby="smoker" name="smoker-buttons">
+                <RadioGroup row aria-labelledby="smoker" name="smoker">
                   <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                   <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
@@ -137,8 +170,8 @@ const MerchantReportForm = ({}) => {
             {/* Marital Status */}
             <Grid>
               <FormControl>
-                <FormLabel id="marital-status">Marital Status</FormLabel>
-                <RadioGroup row aria-labelledby="marital-status" name="marital-status-buttons">
+                <FormLabel id="maritalStatus">Marital Status</FormLabel>
+                <RadioGroup row aria-labelledby="maritalStatus" name="maritalStatus">
                   <FormControlLabel value="married" control={<Radio />} label="Married" />
                   <FormControlLabel value="single" control={<Radio />} label="Single" />
                 </RadioGroup>
@@ -148,8 +181,8 @@ const MerchantReportForm = ({}) => {
             {/* Working Status */}
             <Grid>
               <FormControl>
-                <FormLabel id="working-status">Working Status</FormLabel>
-                <RadioGroup row aria-labelledby="working-status" name="working-status-buttons">
+                <FormLabel id="workingStatus">Working Status</FormLabel>
+                <RadioGroup row aria-labelledby="workingStatus" name="workingStatus">
                   <FormControlLabel value="private" control={<Radio />} label="Private" />
                   <FormControlLabel value="public" control={<Radio />} label="Public" />
                   <FormControlLabel value="student" control={<Radio />} label="Student" />
