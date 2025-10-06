892a2c3b06c7c52bfbe14bd9ba87f8169920cde0
birjd002 <birjd002@mymail.unisa.edu.au>
2025-10-06 20:37:30 +1030

    feat: Add file upload button component

--- client/src/components/ReportUpload.js
+++ client/src/components/ReportUpload.js
@@ -1,13 +1,31 @@
 import {Box, Button, Typography} from '@mui/material'
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import { styled } from '@mui/material/styles';
 
+
+const HiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 const ReportUpload = ({}) => {
   return (
     <Box sx={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
       <Typography sx={{color: 'grey', mb: 4}}>Only PDF (.pdf) or Excel (.xlsx) files are accepted.</Typography>
-      <Button variant='contained' color='info' size='large'>
-        Upload File <FileUploadIcon/>
+      <Button component='label' role='undefined' variant='contained' tabIndex={-1} color='info' size='large' startIcon={<FileUploadIcon/>}>
+        Upload File
+        <HiddenInput
+          type='file'
+          // onChange={(event) => console.log(event.target.files)}
+          multiple
+        />
       </Button>
     </Box>
   )
