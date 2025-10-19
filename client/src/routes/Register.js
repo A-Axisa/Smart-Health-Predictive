e7d5aa5180363de446c9b59003873ab4bc2ddba6
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-10-20 01:46:18 +1030

    chore: Fill entire background with colour

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -3,8 +3,9 @@ import RegistrationForm from '../components/authentication/RegistrationForm'
 
 const Register = ({}) => {
   return (
-    <Container sx={{backgroundColor:'#127067', width:'100vw', 
-      height:'100vh', padding:'0', margin:'0', display:'flex', alignItems:'center', justifyContent:'center'}}  >
+    <Container maxWidth={false} sx={{backgroundColor:'#127067', width:'100vw',
+      height:'100dvh', padding:'0', margin:'0', display:'flex', 
+      alignItems:'center', justifyContent:'center'}}  >
       <RegistrationForm />
     </Container>
   )
