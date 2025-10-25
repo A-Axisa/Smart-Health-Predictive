8b97cdbc9294b0740fb38a8f7729a2511301435c
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-10-25 22:41:42 +1030

    chore: Add logo to login page

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,21 +1,24 @@
 import { Box, Container, Stack, Typography } from '@mui/material'
 import LoginForm from '../components/authentication/LoginForm'
 import WelcomePanel from '../components/WelcomePanel'
+import Logo from '../assets/WellAiLogoTR.png'
 
 const Login = ({}) => {
   return (
-    <Stack direction='row' sx={{ backgroundColor:'#127067',  width:'100vw', height:'100vh', 
-      padding:'0', margin:'0'}}>
+    <Stack direction='row' sx={{ backgroundColor:'#127067'}}>
       <WelcomePanel />
       <Box sx={{px:{xs:0, sm:5, md:20, lg:10, xl:10,}, display: 'flex', 
         alignItems:'center', justifyContent:'center', height:'100vh', 
         backgroundColor:'#efefef', flexGrow:{xs:1, sm:1, md:1, lg:0}}}>
-        <Stack direction={'column'} sx={{px:0, mx:0, flexGrow:{xs:1, sm:0}, 
-          width:{xs:'auto', sm:'500px' }}} >
-          <Container sx={{ display:{ md:'block', lg:'none'}}} >
-            <Typography align='center' sx={{py:3, fontSize:'2.4rem', fontWeight:'bold'}}>
+        <Stack direction={'column'} alignItems={'center'} sx={{px:0, mx:0, 
+          flexGrow:{xs:1, sm:0}, width:{xs:'auto', sm:'500px'}}}>
+          <Box component='img' alt='Well AI Logo' src={Logo} 
+            sx={{padding:{sm:1, lg:5}, width:'20em'}}/>
+          <Container sx={{ display:{ md:'block', lg:'none'}}}>
+            <Typography align='center' sx={{py:3, fontSize:'2.4rem', 
+              fontWeight:'bold'}}>
               Smart Health Predictive
-          </Typography>
+            </Typography>
           </Container>
           <LoginForm/>
         </Stack>
