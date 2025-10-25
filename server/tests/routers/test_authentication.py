7a08ba3b564feb1677c8c1e46fa678bf63286c88
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-10-25 23:53:07 +1030

    fix: Provide valid account details

--- server/tests/routers/test_authentication.py
+++ server/tests/routers/test_authentication.py
@@ -12,7 +12,7 @@ def setup_once_for_all_tests():
     db_conn = next(get_db())
 
     # Delete the account with test credentials to rest for testing
-    prev_account = db_conn.query(UserAccount).filter_by(Email="test@mymail.com").first()
+    prev_account = db_conn.query(UserAccount).filter_by(Email="test@example.com").first()
     if prev_account:
         user_id = prev_account.UserID
 
@@ -24,17 +24,17 @@ def setup_once_for_all_tests():
         db_conn.delete(prev_account)
         db_conn.commit()
         
-
-    credentials = {'username':'Testable User',
-                   'password':'thisisavalidpassword',
-                   'email': 'test@mymail.com',
-                   'phone': '01189998819991197253',
-                   'account_type': 1
-                    }
+    credentials = {
+        'username':'Testable User',
+        'password':'thisisavalidpassword',
+        'email': 'test@example.com',
+        'phone': '',
+        'account_type': 'user'
+    }
     response = client.post('/register/', json=credentials)
 
 def test_login_with_valid_credentials():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     response = client.post('/login/', json=credentials)
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'message': f'HTTP-Only cookie set successfully.'}
@@ -46,7 +46,7 @@ def test_login_with_incorrect_email():
     assert response.json() == {'detail':'Incorrect username or password'}
 
 def test_login_with_incorrect_password():
-    credentials = {'email':'test@mymail.com', 'password':'incorrectpassword'}
+    credentials = {'email':'test@example.com', 'password':'incorrectpassword'}
     response = client.post('/login/', json=credentials)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.json() == {'detail':'Incorrect username or password'}
@@ -64,7 +64,7 @@ def test_login_with_empty_username():
     assert response.json() == {'detail':'Incorrect username or password'}
 
 def test_login_with_empty_password():
-    credentials = {'email':'test@mymail.com', 'password':''}
+    credentials = {'email':'test@example.com', 'password':''}
     response = client.post('/login/', json=credentials)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.json() == {'detail':'Incorrect username or password'}
@@ -81,7 +81,7 @@ def test_login_with_no_username():
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 def test_login_with_no_password():
-    credentials = {'email':'test@mymail.com', 'password':None}
+    credentials = {'email':'test@example.com', 'password':None}
     response = client.post('/login/', json=credentials)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
@@ -97,15 +97,15 @@ def test_sql_injection_password_bypass():
     assert response.json() == {'detail':'Incorrect username or password'}
 
 def test_get_user_matching_email():
-    user = authentication.get_user('test@mymail.com', next(get_db()))
-    assert user.Email == 'test@mymail.com'
+    user = authentication.get_user('test@example.com', next(get_db()))
+    assert user.Email == 'test@example.com'
 
 def test_get_user_not_in_database():
     user = authentication.get_user("", next(get_db()))
     assert user == None
 
 def test_authenticate_user_success():
-    result = authentication.authenticate_user('test@mymail.com', 'thisisavalidpassword', next(get_db()))
+    result = authentication.authenticate_user('test@example.com', 'thisisavalidpassword', next(get_db()))
     assert result
 
 def test_authentication_incorrect_email():
@@ -113,7 +113,7 @@ def test_authentication_incorrect_email():
     assert not result
 
 def test_authentication_incorrect_password():
-    result = authentication.authenticate_user('test@mymail.com', 'incorrectpassword', next(get_db()))
+    result = authentication.authenticate_user('test@example.com', 'incorrectpassword', next(get_db()))
     assert not result
 
 def test_authentication_incorrect_credentials():
@@ -125,7 +125,7 @@ def test_authentication_empty_email():
     assert not result
 
 def test_authentication_empty_password():
-    result = authentication.authenticate_user('test@mymail.com', '', next(get_db()))
+    result = authentication.authenticate_user('test@example.com', '', next(get_db()))
     assert not result
 
 def test_authentication_empty_password():
@@ -138,21 +138,21 @@ def test_authentication_no_email():
 
 def test_authentication_no_password():
     with pytest.raises(AttributeError):
-        authentication.authenticate_user('test@mymail.com', None, next(get_db()))
+        authentication.authenticate_user('test@example.com', None, next(get_db()))
     
 def test_authentication_no_credentials():
     result = authentication.authenticate_user(None, None, next(get_db()))
     assert not result
 
 def test_get_current_user_success():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     client.post('/login/', json=credentials)
     response = client.get('/user/me')
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()['email'] == 'test@mymail.com'
+    assert response.json()['email'] == 'test@example.com'
     
 def test_get_current_user_no_cookie():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     client.post('/login/', json=credentials)
     authentication.invalidate_access_token(credentials['email'], next(get_db()))
     response = client.get('/user/me')
@@ -187,7 +187,7 @@ def test_validate_empty_email():
     assert not result
 
 def test_change_password():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     client.post('/login/', json=credentials)
     change_password = {'current_password':'thisisavalidpassword','new_password':'thisIsSafer','confirm_new_password':'thisIsSafer' }
     response = client.post('/changePassword/',json=change_password)
@@ -195,7 +195,7 @@ def test_change_password():
     assert response.json() == {'message': 'User successfully changed password.'}
 
 def test_change_password_incorrect_current():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     client.post('/login/', json=credentials)
     change_password = {'current_password':'123','new_password':'thisIsSafer','confirm_new_password':'thisIsSafer' }
     response = client.post('/changePassword/',json=change_password)
@@ -203,7 +203,7 @@ def test_change_password_incorrect_current():
 
 
 def test_change_password_not_matching():
-    credentials = {'email':'test@mymail.com', 'password':'thisisavalidpassword'}
+    credentials = {'email':'test@example.com', 'password':'thisisavalidpassword'}
     client.post('/login/', json=credentials)
     change_password = {'current_password':'thisIsSafer','new_password':'123','confirm_new_password':'321' }
     response = client.post('/changePassword/',json=change_password)    
