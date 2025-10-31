0dbc3397b223e44d69be220bac8c751696a95391
Anton Axisa <axiay002@mymail.unisa.edu.au>
2025-10-31 14:25:55 +1030

    doc: Add description and semicolons to register

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -1,6 +1,10 @@
 import { Container} from '@mui/material'
 import RegistrationForm from '../components/authentication/RegistrationForm'
 
+/** 
+ * A page used to display registration information and provide a form to allow
+ * users to register.
+*/
 const Register = ({}) => {
   return (
     <Container maxWidth={false} sx={{backgroundColor:'#127067', width:'100vw',
@@ -8,7 +12,7 @@ const Register = ({}) => {
       alignItems:'center', justifyContent:'center'}}  >
       <RegistrationForm />
     </Container>
-  )
+  );
 }
 
-export default Register
+export default Register;
