6e03bef1efd6760b2832a568c8cb2f0483671c2c
birjd002 <birjd002@mymail.unisa.edu.au>
2025-11-09 18:19:51 +1030

    chore: Refactor test_upload
    NOTE: Untested due to alembic version issues.

--- server/tests/test_upload.py
+++ server/tests/test_upload.py
@@ -10,89 +10,91 @@ app = FastAPI()
 app.include_router(router)
 client = TestClient(app)
 
-test_populated_csv_data = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
+populated_rows = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
 User 1,user1@example.com,0412345678,31,50,1.7,1,4.5,135,120,1,0,1,0,1,0,1,0,1
 User 2,user2@example.com,0812345678,31,60,1.7,1,6,140,120,0,1,1,0,0,1,0,1,1"""
 
-test_one_populated_row_csv_data = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
-,,,,,,,,,,,,,,,,,,
-User 2,user2@example.com,0812345678,31,60,1.7,1,6,140,120,0,1,1,0,0,1,0,1,1"""
-
-test_unpopulated_rows_csv_data = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
+unpopulated_rows = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
 ,,,,,,,,,,,,,,,,,,
 ,,,,,,,,,,,,,,,,,,"""
 
-test_missing_email_csv_data = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
+one_populated_row = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
+,,,,,,,,,,,,,,,,,,
+User 2,user2@example.com,0812345678,31,60,1.7,1,6,140,120,0,1,1,0,0,1,0,1,1"""
+
+missing_email = """FullName,Email,PhoneNumber,Age,WeightInKilograms,HeightCentimetres,Gender,BloodGlucose,APHigh,APLow,HighCholesterol,Exercise,HyperTension,HeartDisease,Diabetes,Alcohol,SmokingStatus,MaritalStatus,WorkingStatus
 User 1,user1@example.com,0412345678,31,50,1.7,1,4.5,135,120,1,0,1,0,1,0,1,0,1
 User 2,,0812345678,31,60,1.7,1,6,140,120,0,1,1,0,0,1,0,1,1"""
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_users_for_tests():
-    db_conn = next(get_db())
 
-    test_merchant = UserAccount(
-        "Merchant User", "merchant@example.com", "validpassword", "1234567890")
-    test_user1 = UserAccount(
-        "User 1", "user1@example.com", "anothervalidpassword", "0412345678")
-    test_user2 = UserAccount(
-        "User 2", "user2@example.com", "yetanothervalidpassword", "0812345678")
+    credentials = [
+        {
+            'username': 'AReputableClinic',
+            'password': 'thisisavalidpassword',
+            'email': 'myreputableclinic@example.com',
+            'phone': '',
+            'account_type': 'merchant'
+        },
+        {
+            'username': 'User 1',
+            'password': 'thisisalongpassword',
+            'email': 'user1@example.com',
+            'phone': '',
+            'account_type': 'user'
+        },
+        {
+            'username': 'User 2',
+            'password': 'thisisareallylongpassword',
+            'email': 'user2@example.com',
+            'phone': '',
+            'account_type': 'user'
+        }
+    ]
 
-    db_conn.add_all([test_merchant, test_user1, test_user2])
-    db_conn.commit()
-    test_merchant.UserID = 123
-    db_conn.commit()
-    yield
+    for account in credentials:
+        client.post("/register/", json=account)
 
-    db_conn.query(UserAccount).filter(
-        UserAccount.Email == "merchant@example.com").delete()
-    db_conn.query(UserAccount).filter(
-        UserAccount.Email == "user1@example.com").delete()
-    db_conn.query(UserAccount).filter(
-        UserAccount.Email == "user2@example.com").delete()
-    db_conn.commit()
+    merchant_credentials = {'email': 'myreputableclinic@example.com',
+                            'password': 'thisisavalidpassword'}
+    
+    client.post("/login/", json=merchant_credentials)
 
+def upload_csv(data: str):
+    file = io.BytesIO(data.encode("utf-8"))
+    encoded_file = {"uploaded_file": ("test.csv", file, "text/csv")}
+    return client.post("/upload/", files=encoded_file)
 
 def test_populated_rows_upload():
-    file = io.BytesIO(test_populated_csv_data.encode("utf-8"))
-    files = {"file": ("test.csv", file, "text/csv")}
-    response = client.post("/upload", files=files,
-                           data={"testMerchantID": 123})
+    response = upload_csv(populated_rows)
+    body = response.json()
+
     assert response.status_code == status.HTTP_200_OK
-
-    data = response.json()
-    assert len(data) == 2
-    assert data[0]["merchantID"] == 123
-    assert data[1]["merchantID"] == 123
-
+    assert body["processed"] == 2
+    assert body["skipped"] == 0
 
 def test_unpopulated_rows_upload():
-    file = io.BytesIO(test_unpopulated_rows_csv_data.encode("utf-8"))
-    files = {"file": ("test.csv", file, "text/csv")}
-    response = client.post("/upload", files=files,
-                           data={"testMerchantID": 123})
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    response = upload_csv(unpopulated_rows)
+    body = response.json()
 
+    assert response.status_code == status.HTTP_200_OK
+    assert body["processed"] == 0
+    assert body["skipped"] == 2
 
 def test_one_populated_row_upload():
-    file = io.BytesIO(test_one_populated_row_csv_data.encode("utf-8"))
-    files = {"file": ("test.csv", file, "text/csv")}
-    response = client.post("/upload", files=files,
-                           data={"testMerchantID": 123})
+    response = upload_csv(one_populated_row)
+    body = response.json()
+
     assert response.status_code == status.HTTP_200_OK
-
-    data = response.json()
-    assert len(data) == 1
-    assert data[0]["merchantID"] == 123
-
+    assert body["processed"] == 1
+    assert body["skipped"] == 1
 
 def test_missing_email_upload():
-    file = io.BytesIO(test_missing_email_csv_data.encode("utf-8"))
-    files = {"file": ("test.csv", file, "text/csv")}
-    response = client.post("/upload", files=files,
-                           data={"testMerchantID": 123})
-    assert response.status_code == status.HTTP_200_OK
+    response = upload_csv(missing_email)
+    body = response.json()
 
-    data = response.json()
-    assert len(data) == 1
-    assert data[0]["merchantID"] == 123
+    assert response.status_code == status.HTTP_200_OK
+    assert body["processed"] == 1
+    assert body["skipped"] == 1
