6683afbf1a9ce5a4855b5f8117cb889ff222b728
Luke Bradley <bralc003@mymail.unisa.edu.au>
2026-03-08 13:01:37 +1030

    Create Standard user Navbar
    Created the navbar for the standard user type with options to navigate to all available pages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,137 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
 
+const NavBar = ({ role }) => {
+  const navigate = useNavigate();
+  // Page options for each user type
+  const standardPages = [
+    "Generate Report",
+    "Health History",
+    "Health Analytics",
+  ];
+  const settings = ["Account", "Logout"];
+  const merchantPages = ["Page"];
+  const adminPages = ["Page"];
+  // Check if settings menu is open
+  const [openMenu, setOpenMenu] = useState(null);
 
-const NavBar = ({role}) => {
+  // Handle navigation for each page option
+  function handleNavigate(page) {
+    if (page === "Generate Report") {
+      navigate("/generate-report");
+    }
+    if (page === "Health History") {
+      navigate("/ai-health-prediction");
+    }
+    if (page === "Health Analytics") {
+      navigate("/health-analytics");
+    }
+    if (page === "Account") {
+      handleCloseSettings();
+      navigate("/user-settings");
+    }
+    if (page === "Logout") {
+      handleCloseSettings();
+      navigate("/user-landing");
+    }
+  }
+  function handleOpenSettings(e) {
+    setOpenMenu(e.currentTarget);
+    console.log(openMenu);
+  }
 
-if (role === "standard_user")
-    return(
-        <h1>Hello User</h1>
+  function handleCloseSettings() {
+    setOpenMenu(null);
+  }
+  // Navigation Bar for standard user type
+  if (role === "standard_user")
+    return (
+      <AppBar position="static">
+        <Toolbar disableGutters>
+          {/*Title/Logo Section*/}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              justifyContent: "flex-start",
+              pl: 2,
+            }}
+          >
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              onClick={() => navigate("/user-landing")}
+              sx={{
+                color: "inherit",
+                textDecoration: "none",
+                cursor: "pointer",
+              }}
+            >
+              Smart Health Predictive
+            </Typography>
+          </Box>
+          {/*Page Options  */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {standardPages.map((page) => (
+              <Button
+                key={page}
+                onClick={() => handleNavigate(page)}
+                sx={{ my: 2, color: "inherit", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          {/*Account Settings Section  */}
+          <Box sx={{ flexGrow: 0 }}>
+            <Button color="inherit">
+              <AccountCircleIcon
+                onClick={handleOpenSettings}
+                sx={{ p: 0 }}
+              ></AccountCircleIcon>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={openMenu}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(openMenu)}
+                onClose={handleCloseSettings}
+              >
+                {settings.map((page) => (
+                  <MenuItem key={page} onClick={() => handleNavigate(page)}>
+                    <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
     );
 
-if (role === "merchant")
-    return(
-        <h1>Hello Merchant</h1>
-    );
-
-if (role === "admin")
-return(
-        <h1>Hello Admin</h1>
-    );
-}
-
+  // Navigation Bar for merchant user type
+  if (role === "merchant") return <h1>Hello Merchant</h1>;
+  // Navigation Bar for admin user type
+  if (role === "admin") return <h1>Hello Admin</h1>;
+};
 
 export default NavBar;
