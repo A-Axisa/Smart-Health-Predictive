bea962aa94a019587737697d357b2e4ccaf1513f
Anton Axisa <axiay002@mymail.unisa.edu.au>
2026-03-11 14:02:38 +1030

    Add file for front-end blood report component

---
+++ client/src/components/BloodReportUpload.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material'
+
+const BloodReportUpload = ({}) => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default BloodReportUpload;
