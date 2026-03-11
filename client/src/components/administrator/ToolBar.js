24e06b2a0f5910c5f371381a2ae5bd20e2a2c45a
birjd002 <birjd002@mymail.unisa.edu.au>
2026-03-12 02:31:25 +1030

    update: Updated toolbar with role change and deletion

--- client/src/components/administrator/ToolBar.js
+++ client/src/components/administrator/ToolBar.js
@@ -1,12 +1,41 @@
-import { Box } from '@mui/x-data-grid';
+import { Box, Select, Button, MenuItem, Typography } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-const ToolBar = ({  }) => {
 
+const ToolBar = ({ emailCount, onUsersDelete, onUsersRoleChange, roleData }) => {
   return (
-    <Box>
-      
+    <Box sx={{ p: 1, display: 'flex', alignItems: 'center', gap: 2, justifyContent: 'flex-end' }}>
+      <Select
+        size="small"
+        displayEmpty
+        disabled={emailCount === 0}
+        value=""
+        onChange={(e) => onUsersRoleChange(e.target.value)}
+        sx={{ minWidth: 150 }}
+      >
+        <MenuItem
+          value=""
+          disabled
+        >
+          Change Role...
+        </MenuItem>
+        {roleData.map((role) => (
+          <MenuItem key={role.id} value={role.id}>
+            {role.name}
+          </MenuItem>
+        ))}
+      </Select>
+      <Button
+        size="small"
+        color="error"
+        variant="contained"
+        disabled={emailCount === 0}
+        onClick={onUsersDelete}
+      >
+        <DeleteForeverIcon /> Delete Selected
+      </Button>
     </Box>
   );
-}
+};
 
 export default ToolBar
