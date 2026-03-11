c4d07f63eb5c5235144826388e907cb8352a1d8e
Anton Axisa <axiay002@mymail.unisa.edu.au>
2026-03-11 14:52:01 +1030

    Add button to blood report upload component

--- client/src/components/BloodReportUpload.js
+++ client/src/components/BloodReportUpload.js
@@ -1,10 +1,36 @@
-import { Container } from '@mui/material'
+import { Button, Container } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 const BloodReportUpload = ({}) => {
-    return (
-        <Container>
 
-        </Container>
+  const HiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
+    return (
+      <Container>
+        <Button
+          component="label"
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<FileUploadIcon />}
+        >
+          Upload Blood Report
+          <HiddenInput
+            type="file"
+            onChange={(event) => console.log(event.target.files)}
+          />
+        </Button>
+      </Container>
     )
 }
 
