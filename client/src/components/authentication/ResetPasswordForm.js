77e93a6f2328e46b535760e9055b242a0f7b1a4c
Anton Axisa <axiay002@mymail.unisa.edu.au>
2026-04-02 21:26:17 +1030

    Add input fields and button to password reset form

--- client/src/components/authentication/ResetPasswordForm.js
+++ client/src/components/authentication/ResetPasswordForm.js
@@ -1,14 +1,42 @@
-import { Container } from "@mui/material";
+import { useState } from "react";
 import {
+  Box,
   Container,
   Stack,
+  Button,
   Typography,
+  Link,
+  TextField,
 } from "@mui/material";
+import PasswordInputField from "../authentication/PasswordInputField";
 
 /**
  * Provides a form to reset their password one time with a reset token.
  */
 const ResetPasswordForm = () => {
+  const [alertPasswordRequired, setAlertPasswordRequired] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState(null);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [alertPasswordsDontMatch, setAlertPasswordsDontMatch] = useState(false);
+
+  function validatePassword(e) {
+    setAlertPasswordRequired(false);
+    setIsPasswordValid(e.isValid);
+    setPassword(e.password);
+  }
+
+  function updateConfirmPassword(e) {
+    const confirmPasswordInput = e.target.value;
+    setConfirmPassword(confirmPasswordInput);
+    setAlertPasswordsDontMatch(
+      confirmPasswordInput !== password || confirmPasswordInput === "",
+    );
+  }
+
+  async function handleSubmit(e) {}
+
   return (
     <Container
       sx={{
@@ -21,7 +49,7 @@ const ResetPasswordForm = () => {
         flexGrow: { xs: 1, sm: 0 },
       }}
     >
-      {/* Password form */}
+      {/* Input form */}
       <Stack
         direction="column"
         spacing={{ xs: 5 }}
@@ -30,11 +58,44 @@ const ResetPasswordForm = () => {
         style={{ justifyContent: "center" }}
       >
         <Stack direction="column" spacing={{ xs: 2 }}>
-          <h1>Account Recovery</h1>
+          <h1>Reset Password</h1>
           <Typography align="start" style={{ color: "#777777" }}>
-            Enter your new password below and submit to update your password
+            Enter your new password below and submit to update your password.
           </Typography>
         </Stack>
+        <Box component="form" onSubmit={handleSubmit}>
+          <Stack spacing={{ xs: 2 }}>
+            <PasswordInputField
+              onChange={validatePassword}
+              truncate={true}
+              showRequired={alertPasswordRequired}
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Confirm Password"
+              onChange={updateConfirmPassword}
+              type="password"
+              error={alertPasswordsDontMatch}
+              helperText={
+                alertPasswordsDontMatch ? "*Passwords do not match" : null
+              }
+            ></TextField>
+            <Button
+              loading={isLoading}
+              type="submit"
+              variant="contained"
+              sx={{
+                py: { xs: "1rem", sm: ".9rem" },
+                fontSize: { xs: "1.2rem", sm: "1rem" },
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Box>
+        <Link href="/login" align="end" fontWeight="bold">
+          Cancel
+        </Link>
       </Stack>
     </Container>
   );
