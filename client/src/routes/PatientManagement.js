296bd2f99abb206ceec07660a2bcc19ee38a1f74
Luke Bradley <bralc003@mymail.unisa.edu.au>
2026-04-03 13:11:38 +1030

    Add patient management route

---
+++ client/src/routes/PatientManagement.js
@@ -0,0 +1,19 @@
+import { Box, Typography } from "@mui/material";
+
+/**
+ * A page used to display a list of all patients for a merchant user.
+ */
+const PatientManagement = () => {
+  return (
+    <Box
+      sx={{
+        ml: "250px",
+        mt: "66px",
+      }}
+    >
+      <Typography>Patients</Typography>
+    </Box>
+  );
+};
+
+export default PatientManagement;
