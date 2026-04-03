bdfd3f3fc124f330d523e59063f1a6769cd8f369
Anton Axisa <axiay002@mymail.unisa.edu.au>
2026-04-03 17:32:16 +1030

    fix: Pass user.UserAccount into method

--- server/routers/authentication.py
+++ server/routers/authentication.py
@@ -597,6 +597,7 @@ def forgot_password(forgot_password_request: ForgotPasswordRequest, request: Req
             db_conn.add(pass_reset_token)
             db_conn.commit()
             is_success = True
+            _send_reset_password_email(user.UserAccount, patient, request, token)
 
     write_audit_log(
         db_conn,
