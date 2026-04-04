5cf378aaf3f33b2b106e15f40b19f4972ce988fe
Luke Bradley <bralc003@mymail.unisa.edu.au>
2026-04-04 16:00:56 +1030

    Add remove button to patient management table

--- client/src/routes/PatientManagement.js
+++ client/src/routes/PatientManagement.js
@@ -3,6 +3,8 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import DeleteIcon from "@mui/icons-material/Delete";
+
 /**
  * A page used to display a list of all patients for a merchant user.
  */
@@ -16,14 +18,14 @@ const PatientManagement = () => {
     {
       field: "givenNames",
       headerName: "Given Names",
-      width: 250,
+      flex: 1,
       sortable: true,
     },
-    { field: "lastName", headerName: "Last Name", width: 250, sortable: true },
+    { field: "lastName", headerName: "Last Name", flex: 1, sortable: true },
     {
       field: "dateOfBirth",
       headerName: "Date of Birth",
-      width: 150,
+      width: 120,
       sortable: true,
     },
     {
@@ -32,9 +34,32 @@ const PatientManagement = () => {
       width: 100,
       sortable: true,
     },
+    {
+      field: "remove",
+      headerName: "",
+      width: 100,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params) => (
+        <Button
+          color="error"
+          onClick={(e) => {
+            e.stopPropagation();
+            handleDelete(params.id);
+          }}
+        >
+          <DeleteIcon />
+        </Button>
+      ),
+    },
   ];
 
   useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = () => {
     fetch(`${API_BASE}/merchant/associated-patients`, {
       method: "GET",
       credentials: "include",
@@ -47,16 +72,25 @@ const PatientManagement = () => {
           lastName: patient.family_name,
           gender: patient.gender === 0 ? "Female" : "Male",
           dateOfBirth: new Date(patient.date_of_birth).toLocaleDateString(
-            "Aus",
+            "en-AU",
           ),
         }));
-
         setPatientData(patients);
       })
+      .catch(console.log);
+  };
+
+  async function handleDelete(patientID) {
+    await fetch(`${API_BASE}/remove-patient/${patientID}`, {
+      method: "DELETE",
+      credentials: "include",
+    })
+      .then((response) => response.json())
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+    fetchPatients();
+  }
 
   return (
     <Box
