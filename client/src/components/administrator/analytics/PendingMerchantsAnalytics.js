d392eece1688d4396e59119296679e6a7183d46d
A-Axisa <a3132688@adelaide.edu.au>
2026-05-03 15:46:20 +0930

    Add notification icon to pending merchants

--- client/src/components/administrator/analytics/PendingMerchantsAnalytics.js
+++ client/src/components/administrator/analytics/PendingMerchantsAnalytics.js
@@ -2,6 +2,7 @@ import { Card, CardContent, Container, Paper, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import { useEffect, useState } from "react";
 import Divider from "@mui/material/Divider";
+import ErrorIcon from "@mui/icons-material/Error";
 
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
@@ -32,9 +33,16 @@ const PendingMerchantsAnalytics = () => {
   return (
     <Card sx={{ px: "5px", py: "0px", textAlign: "center" }}>
       <CardContent>
-        <Stack direction="row" alignItems="center" spacing={1}>
-          <Typography variant="h4">{data.amount}</Typography>
-          <Typography variant="h7">Merchants awaiting review</Typography>
+        <Stack direction="row">
+          <Stack direction="row" justifyContent="space-around">
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Typography variant="h4">{data.amount}</Typography>
+              <Typography variant="h7">Merchants awaiting review</Typography>
+            </Stack>
+            {data.amount > 0 && (
+              <ErrorIcon sx={{ fontSize: 40, color: "red" }} />
+            )}
+          </Stack>
         </Stack>
       </CardContent>
     </Card>
