86fdcfbb392b47cf1663954219f6fe8f5d7cba6b
birjd002 <birjd002@mymail.unisa.edu.au>
2026-05-19 22:02:13 +0930

    update: reorder lifestyle section in ReportTemplate

--- client/src/components/ReportTemplate.js
+++ client/src/components/ReportTemplate.js
@@ -272,16 +272,6 @@ const ReportTemplate = ({ report, date }) => {
             p: 2,
           }}
         >
-          {/* Lifestyle */}
-          <Box>
-            <Typography variant={isMobile ? "h5" : "h4"} sx={{ mb: 1 }}>
-              Lifestyle Recommendations
-            </Typography>
-            <Typography variant="body1">
-              {report.lifestyleRecommendation ||
-                "No lifestyle recommendation available."}
-            </Typography>
-          </Box>
 
           {/* Exercise */}
           <Box>
@@ -301,6 +291,17 @@ const ReportTemplate = ({ report, date }) => {
             </Typography>
             <Typography variant="body1">
               {report.dietRecommendation || "No diet recommendation available."}
+            </Typography>
+          </Box>
+
+          {/* Lifestyle */}
+          <Box>
+            <Typography variant={isMobile ? "h5" : "h4"} sx={{ mb: 1 }}>
+              Lifestyle Recommendations
+            </Typography>
+            <Typography variant="body1">
+              {report.lifestyleRecommendation ||
+                "No lifestyle recommendation available."}
             </Typography>
           </Box>
 
